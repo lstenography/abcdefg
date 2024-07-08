0bbb6e1523aa2e8dcf30f72517517994a981d072
jhonathanenhathane <jhonathanenhathane@gmail.com>
2024-07-08 21:02:40 +0000

    issue

--- scripts/circuits.js
+++ scripts/circuits.js
@@ -1,4 +1,6 @@
 
+
+
 function changeTheme() {
     const body = document.body;
     const dark_theme = document.getElementById("dark-theme");
@@ -7,6 +9,9 @@ function changeTheme() {
     if(body.classList.contains(dark_theme)){
         body.classList.add(light_theme);
         body.classList.remove(dark_theme);
+    } else {
+        body.classList.add(dark_theme);
+        body.classList.remove(light_theme);
     }
 }
 
